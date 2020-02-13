a1b826ce4318743149c94d01196a40cd4ffec1cb
Roma <silvervaza@gmail.com>
2020-02-13 22:37:35 +0200

    add Readme

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('file:create');
+        $schedule->command('file:create')->cron('*/11 * * * *');
     }
 
     /**
